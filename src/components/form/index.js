1a8f9fb24be117838e0ef79e03f8df8aa8000326
Sean cehn <sean@nexusvisa.ca>
2022-04-07 20:18:32 -0600

    formik + yup + material UI

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import { getIn, useFormik } from 'formik';
+import { useFormik } from 'formik';
 import * as yup from 'yup';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
-const signUpForm = () => {
-
+const SignUpForm = () => {
     const validateSchema = yup.object({
         fullname: yup
             .string('Enter your full name')
@@ -14,41 +13,79 @@ const signUpForm = () => {
         age: yup
             .number('Enter your age')
             .required('Age is required')
+            .typeError('Please enter digital numbers')
             .lessThan(150, 'the max number of age is 150')
-            .moreThan(18, 'The min number of age is 18')
-            .positive('The number of age must be a positive number'),
+            .moreThan(17, 'The min number of age is 18'),
         email: yup
             .string('Enter your email')
-            .email()
             .required('Email is required')
-            .max(50, 'The max length of email is 50 characters'),
-        website: yup
-            .string('Enter your website')
-            .url()
-            .required('Enter youe website')
-            .max(200, 'The max length of website is 200 characters')
+            .max(50, 'The max length of email is 50 characters')
+            .email('Enter a valid email'),
     });
 
     const formik = useFormik({
         initialValues: {
-            age: 18
+            fullname: 'John Doe',
+            age: '18',
+            email: 'example@abc.com',
         },
         validationSchema: validateSchema,
         onSubmit: values => {
             console.log(values);
-            alter(JSON.stringify(values, null, 2));
+            alert(JSON.stringify(values, null, 2));
         },
-    })
-
+    });
+    
     return (
-        <form onSubmit={formik.handleSubmit}>
+        <div>
+            <form onSubmit={formik.handleSubmit}>
+                <TextField
+                    fullWidth
+                    id="fullname"
+                    name="fullname"
+                    label="Fullname"
+                    value={formik.values.fullname}
+                    onChange={formik.handleChange}
+                    error={formik.touched.fullname && Boolean(formik.errors.fullname)}
+                    helperText={formik.touched.fullname && formik.errors.fullname}
+                />
+                <TextField
+                    fullWidth
+                    id="age"
+                    name="age"
+                    label="Age"
+                    value={formik.values.age}
+                    onChange={formik.handleChange}
+                    error={formik.touched.age && Boolean(formik.errors.age)}
+                    helperText={formik.touched.age && formik.errors.age}
+                />
 
-            //to do 
+                <TextField
+                    fullWidth
+                    id="email"
+                    name="email"
+                    label="Email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
+                />
 
-        <button type="submit">Submit</button>
-        </form>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                >
+                    Submit
+                </Button>
+
+            </form>
+        </div>
+
     );
 
 };
 
 
+export default SignUpForm;
